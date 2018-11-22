cfc8025eb2a4b85a9772050d94abc5d3caee9a82
Jean Benitez <jkbdiaz@gmail.com>
2018-11-21 23:31:09 -0500

    addign support to slack webhook test (4)

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ app.post('/slack_webhook', (req, res) => {
   const { channel, user, type, challenge } = req.body;
 
   if (type === 'url_verification') {
-    return res.status(400).send({ challenge });
+    return res.status(200).send({ challenge });
   }
 
   if (channel === channelId && type === 'member_left_channel') {
     const url = `https://slack.com/api/channels.invite`;
     const extraHeader = { 'Authorization': 'Bearer ' + token };
     fetch(url, makeBody({ channel, user }, extraHeader));
-    return res.status(400).send({ msg: 'DONE' });
+    return res.status(200).send({ msg: 'DONE' });
   }
 
-  return res.status(400).send({ msg: 'DONE' });
+  return res.status(200).send({ msg: 'DONE' });
 });
 
 const PORT = process.env.PORT || 3000;
